5bda3666cefa36e29ba9b88db1ace1d3b4adbefa
liguang <15607521232@163.com>
2022-07-18 00:35:33 +0800

    extentreport

---
+++ Chapter6/src/main/java/com/course/code/extentdemo/TestExtentRep.java
@@ -0,0 +1,30 @@
+package com.course.code.extentdemo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestExtentRep {
+
+    @Test
+    public void testFalse(){
+        Assert.assertEquals(1,2);
+    }
+
+
+    @Test
+    public void testTrue(){
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void testString(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void testLog(){
+        Reporter.log("这是我们自己写的日志");
+        throw new RuntimeException("这是我们自己运行时的异常");
+    }
+}
