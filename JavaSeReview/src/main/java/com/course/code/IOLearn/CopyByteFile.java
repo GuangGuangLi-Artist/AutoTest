1ce5881784e0c9d0d7aaa0a4afcab584352bb9ba
liguang <15607521232@163.com>
2022-09-23 23:21:48 +0800

    IO复习测试

---
+++ JavaSeReview/src/main/java/com/course/code/IOLearn/CopyByteFile.java
@@ -0,0 +1,96 @@
+package com.course.code.IOLearn;
+
+
+import org.testng.annotations.Test;
+
+import java.io.*;
+
+/**
+ * 复习IO流复制文件 读写二进制文件的操作 例如图片
+ */
+public class CopyByteFile {
+
+
+    //基本字节流一次读取一个字节
+
+    @Test
+    public void byteByOneByte() throws IOException {
+
+        File src = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED.gif");
+        File desc = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED_byteByOneByte.gif");
+
+        FileInputStream fis = new FileInputStream(src);
+        FileOutputStream fos = new FileOutputStream(desc);
+
+        int by =0;
+        while ((by=fis.read()) != -1){
+            fos.write(by);
+        }
+
+        fis.close();
+        fos.close();
+
+    }
+
+    //基本字节流一次读取一个字节数组
+
+    @Test
+    public void byteByByteArr() throws IOException {
+
+        File src = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED.gif");
+        File desc = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED_byteByByteArr.gif");
+
+        FileInputStream fis = new FileInputStream(src);
+        FileOutputStream fos = new FileOutputStream(desc);
+
+        byte [] bys = new byte[1024];
+        int len = 0;
+        while ((len=fis.read(bys)) != -1){
+            fos.write(bys,0,len);
+        }
+
+        fis.close();
+        fos.close();
+    }
+
+    //字节缓冲流一次读取一个字节
+
+    @Test
+    public void bufferByteByOneByte() throws IOException {
+        File src = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED.gif");
+        File desc = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED_bufferByteByOneByte.gif");
+
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(src));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(desc));
+
+        int by = 0;
+        while ((by= bis.read()) != -1){
+            bos.write(by);
+        }
+
+        bis.close();
+        bos.close();
+    }
+
+    //字节缓冲流一次读取一个字节数组
+    @Test
+    public void bufferByteByByteArr() throws IOException {
+        File src = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED.gif");
+        File desc = new File("F:\\ideaWorkspace\\AutoTest\\JavaSeReview\\src\\main\\java\\FileByteCopy\\HANGSEED_bufferByteByByteArr.gif");
+
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(src));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(desc));
+
+        byte[] bys = new byte[1024];
+        int len = 0;
+        while ((len=bis.read(bys)) != -1){
+            bos.write(bys,0,len);
+        }
+
+        bis.close();
+        bos.close();
+
+    }
+
+
+}
