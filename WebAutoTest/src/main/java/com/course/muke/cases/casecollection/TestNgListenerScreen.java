8b96b35e833f61230addf0bbaa4fb910004b1a11
liguang <940102569@qq.com>
2022-10-08 13:44:19 +0800

    PageObject 新增监听截图和log4j

--- WebAutoTest/src/main/java/com/course/muke/cases/casecollection/TestNgListenerScreen.java
+++ WebAutoTest/src/main/java/com/course/muke/cases/casecollection/TestNgListenerScreen.java
@@ -1,13 +1,50 @@
 package com.course.muke.cases.casecollection;
 
+import com.course.pageobject.cases.LoginCase;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
+import org.testng.reporters.Files;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TestNgListenerScreen extends TestListenerAdapter {
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
         System.out.println("case执行失败");
+        LoginCase lc = (LoginCase)iTestResult.getInstance();
+        WebDriver driver = lc.driver;
+        try {
+            this.takeScreenShot(driver);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         super.onTestFailure(iTestResult);
+    }
+
+    public void takeScreenShot(WebDriver driver) throws FileNotFoundException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_ss");
+        String curTime = dateFormat.format(new Date());
+
+        //String clsName = this.getClass().getName();
+        String pngName = curTime + ".png";
+
+        String curPath = System.getProperty("user.dir");
+        System.out.println(curPath);
+        File logFile = ((RemoteWebDriver)driver).getScreenshotAs(OutputType.FILE);
+        FileInputStream fis = new FileInputStream(logFile);
+        try {
+            Files.copyFile(fis,new File("src/main/java/com/course/muke/cases/log/" + pngName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- WebAutoTest/src/main/java/com/course/pageobject/cases/LoginCase.java
+++ WebAutoTest/src/main/java/com/course/pageobject/cases/LoginCase.java
@@ -1,23 +1,31 @@
 package com.course.pageobject.cases;
 
+import com.course.muke.cases.casecollection.TestNgListenerScreen;
 import com.course.pageobject.handle.LoginHandle;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(TestNgListenerScreen.class)
 public class LoginCase {
 
     public WebDriver driver;
     public LoginHandle loginHandle;
+    public static Logger logger =Logger.getLogger(LoginCase.class);
 
     @BeforeClass
     public void beforeLogin(){
 
 
+        PropertyConfigurator.configure("src/main/java/com/course/muke/cases/config/log4j.properties");
+        logger.debug("初始化浏览器");
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://www.imooc.com/user/newlogin");
