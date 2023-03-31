50fc7e3711126d6932ba4efc0e799b0ccbb464cd
liguang <15607521232@163.com>
2023-04-01 00:22:49 +0800

    正则表达式 可能能成功了呀

--- HuaWeiSuanFa/src/main/java/com/course/code/RegexLearn.java
+++ HuaWeiSuanFa/src/main/java/com/course/code/RegexLearn.java
@@ -12,16 +12,30 @@ public class RegexLearn {
 
         //String rex = "(\\[img\\])*";
         String rex = "(\\[img\\D{4}\\d{6,8}\\D*\\])*";
-        String oldContent = "[img::::9859025:::|style=&quot;&quot;:::][img::::9859027:::|style=&quot;&quot;:::]";
+        String oldContent = "[img::::9859025:::|style=&quot;&quot;:::][img::::9859027:::|style=&quot;&quot;:::]" +
+                "[img::::9859029:::|style=&quot;&quot;:::]" +
+                "[img::::9859031:::|style=&quot;&quot;:::][img::::9859033:::|style=&quot;&quot;:::]";
         Pattern pattern = Pattern.compile(rex);
         Matcher matcher = pattern.matcher(oldContent);
+        String newfir = "";
         if(matcher.matches()){
-            String con = matcher.group(0);
-            String con1 = matcher.group(1);
-            System.out.println(con);
-            System.out.println(con1);
+            newfir = matcher.group(0);
+
         }else {
             System.out.println("匹配不到");
         }
+        //String newRex = "((\\[img\\D{4})(\\d{7})(\\D*\\]))*";
+        //String newRex = "\\[(.*?)]";
+        String newRex = "\\[img\\:\\:\\:\\:(\\d{7})\\:\\:\\:\\|style\\=\\&quot\\;\\&quot\\;\\:\\:\\:\\]";
+        Pattern pattern1 = Pattern.compile(newRex);
+        Matcher matcher1 = pattern1.matcher(newfir);
+        if(matcher1.matches()){
+            System.out.println(matcher1.group(0));
+        }else {
+            System.out.println("匹配失败");
+        }
+
+        //System.out.println(matcher.groupCount());
+
     }
 }
