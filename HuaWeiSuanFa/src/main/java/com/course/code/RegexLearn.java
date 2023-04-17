e5fce92f8b2bd95c7b5301f14eb484411b591092
liguang <15607521232@163.com>
2023-04-17 23:51:11 +0800

    添加博客

--- HuaWeiSuanFa/src/main/java/com/course/code/RegexLearn.java
+++ HuaWeiSuanFa/src/main/java/com/course/code/RegexLearn.java
@@ -8,34 +8,23 @@ import java.util.regex.Pattern;
 public class RegexLearn {
 
     @Test
-    public void test(){
+    public void testRegex(){
 
         //String rex = "(\\[img\\])*";
-        String rex = "(\\[img\\D{4}\\d{6,8}\\D*\\])*";
+        String rex = "\\d{7}";
         String oldContent = "[img::::9859025:::|style=&quot;&quot;:::][img::::9859027:::|style=&quot;&quot;:::]" +
                 "[img::::9859029:::|style=&quot;&quot;:::]" +
                 "[img::::9859031:::|style=&quot;&quot;:::][img::::9859033:::|style=&quot;&quot;:::]";
         Pattern pattern = Pattern.compile(rex);
         Matcher matcher = pattern.matcher(oldContent);
         String newfir = "";
-        if(matcher.matches()){
-            newfir = matcher.group(0);
+        while (matcher.find()){
+            newfir = matcher.group();
+            System.out.println(newfir);
 
-        }else {
-            System.out.println("匹配不到");
-        }
-        //String newRex = "((\\[img\\D{4})(\\d{7})(\\D*\\]))*";
-        //String newRex = "\\[(.*?)]";
-        String newRex = "\\[img\\:\\:\\:\\:(\\d{7})\\:\\:\\:\\|style\\=\\&quot\\;\\&quot\\;\\:\\:\\:\\]";
-        Pattern pattern1 = Pattern.compile(newRex);
-        Matcher matcher1 = pattern1.matcher(newfir);
-        if(matcher1.matches()){
-            System.out.println(matcher1.group(0));
-        }else {
-            System.out.println("匹配失败");
         }
 
-        //System.out.println(matcher.groupCount());
+
 
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AutoTest
 APIAutoTest
+* 华为机试高频题目:https://blog.csdn.net/weixin_44052055/article/details/123930856
