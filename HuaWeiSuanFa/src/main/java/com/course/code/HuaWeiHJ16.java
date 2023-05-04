ba4bb2408e5ab890c61c54931106b54357d276e9
liguang <15607521232@163.com>
2023-05-04 22:22:18 +0800

    查找兄弟单词

--- HuaWeiSuanFa/src/main/java/com/course/code/HuaWeiHJ16.java
+++ HuaWeiSuanFa/src/main/java/com/course/code/HuaWeiHJ16.java
@@ -15,35 +15,32 @@ import java.util.Scanner;
 public class HuaWeiHJ16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt() / 10; //总钱数，由于是10的倍数，所以先除以10，节省空间
+        int N = sc.nextInt() / 10; // 总钱数，由于是10的倍数，所以先除以10，节省空间
         int m = sc.nextInt();// 总物品数目
-
-        int [][] prices = new int[N + 1][3];
-        int [][] weights = new int[m + 1][3];
+        int[][] prices = new int[N + 1][3];
+        int[][] weights = new int[m + 1][3];
         sc.nextLine();
 
-        for (int i = 1; i <= m ; i++) {
-            int v = sc.nextInt() / 10 ;// 需要的钱，由于是10的倍数，所以先除以10，
-            int p = sc.nextInt() * v;  // 钱*重要度
+        for (int i = 1; i <= m; i++) {
+            int v = sc.nextInt() / 10; // 需要的钱，由于是10的倍数，所以先除以10，
+            int p = sc.nextInt() * v;// 钱*重要度
             int q = sc.nextInt();
             sc.nextLine();
-            if(q==0){// 主件直接加就行
+            if (q == 0) {// 主件直接加就行
                 prices[i][0] = v;
                 weights[i][0] = p;
-            }else if(prices[q][1] == 0){// 从件先试着赋值到主件的第一个位置去
+            } else if (prices[q][1] == 0) {// 从件先试着赋值到主件的第一个位置去
                 prices[q][1] = v;
                 weights[q][1] = p;
-            }else {
+            } else {// 1号有从件后赋值到从件2
                 prices[q][2] = v;
                 weights[q][2] = p;
             }
         }
 
         int[] dp = new int[N + 1];// 压缩空间到1维
-
-        for (int i = 1; i <m ; i++) {// 遍历所有物品
-            for (int j = N; j >= 1 ; j--) {
-
+        for (int i = 1; i <= m; i++) {// 遍历所有物品
+            for (int j = N; j >= 1; j--) {// 从后向前遍历防止出错
                 // 分为四种情况：只放主，放主+从1，放主+从2，放主+从1+从2
                 int p1 = prices[i][0];// 从件都为0的
                 int w1 = weights[i][0];
@@ -51,7 +48,6 @@ public class HuaWeiHJ16 {
                 int w2 = weights[i][1];
                 int p3 = prices[i][2];
                 int w3 = weights[i][2];
-
 
                 if (j - p1 >= 0) {
                     dp[j] = Math.max(dp[j],	dp[j - p1] + w1);
@@ -65,12 +61,11 @@ public class HuaWeiHJ16 {
                 if (j - p1 - p2 - p3 >= 0) {
                     dp[j] = Math.max(dp[j],	dp[j - p1 - p2 - p3] + w1 + w2 + w3);
                 }
-
             }
-
         }
 
         System.out.println(dp[N] * 10);// 由于是10的倍数，所以乘以10
+
         sc.close();
     }
 
