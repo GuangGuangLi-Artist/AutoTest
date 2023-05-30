da688ce1740762a2620bbf877f454f67c5ab653d
liguang <15607521232@163.com>
2023-05-31 00:05:49 +0800

    序列化 数据乱码

---
+++ JavaSeReview/src/main/java/com/course/code/functionalProgramming/LambdaCollections.java
@@ -0,0 +1,73 @@
+package com.course.code.functionalProgramming;
+
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Collection:removeIf() spliterator() stream() parallelStream() forEach()
+ * List:replaceAll() sort()
+ * Map:getOrDefault() forEach() replaceAll() putIfAbsent() remove() replace() computeIfAbsent() computeIfPresent() compute() merge()
+ */
+public class LambdaCollections {
+
+
+
+
+
+    @Test
+    public void testList_forEach() {
+        //forEach() 作用是对容器中的每个元素执行action指定的动作
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("I", "love", "you", "too"));
+        list.forEach(s -> {
+            if(s.length() >= 3){
+                System.out.println("forEach(): " + s);
+            }
+        });
+    }
+
+    @Test
+    public void testList_removeIf() {
+        //removeIf() 作用是删除容器中所有满足filter指定条件的元素
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("I", "love", "you", "too"));
+        list.removeIf(s -> s.length() > 3);
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testList_replaceAll(){
+        //replaceAll() 作用是对每个元素执行operator指定的操作，并用操作结果来替换原来的元素。
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("I", "love", "you", "too"));
+        list.replaceAll(s -> {
+            if(s.length() >= 3){
+                 return s.toUpperCase();
+            }
+            return s;
+        });
+
+        list.forEach(s -> {
+            System.out.println(s);
+        });
+    }
+
+    @Test
+    public void testList_sort(){
+        //sort() 该方法根据c指定的比较规则对容器元素进行排序。
+        ArrayList<String> list = new ArrayList<>(Arrays.asList("I", "love", "you", "too"));
+        list.sort((str1,str2) -> str1.length() - str2.length());
+
+        list.forEach(System.out::println);
+    }
+
+    public void testList_spliterator(){
+
+        //spliterator() 该方法返回容器的可拆分迭代器
+        //1.Spliterator既可以像Iterator那样逐个迭代，也可以批量迭代。批量迭代可以降低迭代的开销。
+        //2.Spliterator是可拆分的，一个Spliterator可以通过调用Spliterator<T> trySplit()方法来尝试分成两个。一个是this，另一个是新返回的那个，这两个迭代器代表的元素没有重叠。
+    }
+
+
+
+}
