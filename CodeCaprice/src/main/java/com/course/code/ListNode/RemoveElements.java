2ba821d3da98c8edfaa1812827c17b1eadb2d393
liguang <15607521232@163.com>
2023-08-17 23:36:27 +0800

    CodeCaprice BinarySearch

--- CodeCaprice/src/main/java/com/course/code/ListNode/RemoveElements.java
+++ CodeCaprice/src/main/java/com/course/code/ListNode/RemoveElements.java
@@ -10,4 +10,29 @@ package com.course.code.ListNode;
  */
 public class RemoveElements {
 
+    public static void main(String[] args) {
+
+    }
+
+
+    //添加虚拟节点
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1,head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+
+        while (cur != null) {
+            if(cur.val == val){
+                pre.next = cur.next;
+            }else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
 }
