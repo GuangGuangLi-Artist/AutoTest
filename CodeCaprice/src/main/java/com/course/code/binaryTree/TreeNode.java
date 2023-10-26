d9f3a91bad2432ba397cc23bf739d8aba890bb48
liguang <15607521232@163.com>
2023-10-27 00:19:08 +0800

    codeCaprice tree

---
+++ CodeCaprice/src/main/java/com/course/code/binaryTree/TreeNode.java
@@ -0,0 +1,22 @@
+package com.course.code.binaryTree;
+
+/**
+ * 这是一个二叉树的定义
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){};
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
