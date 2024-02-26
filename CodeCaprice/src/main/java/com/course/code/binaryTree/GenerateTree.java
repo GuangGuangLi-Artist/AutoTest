be8498a08b1541974caddaabe47959e263a7963a
liguang <15607521232@163.com>
2024-02-26 22:53:49 +0800

    codeCaprice

--- CodeCaprice/src/main/java/com/course/code/binaryTree/GenerateTree.java
+++ CodeCaprice/src/main/java/com/course/code/binaryTree/GenerateTree.java
@@ -4,12 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 106 从中序与后序遍历序列构造二叉树
+ * 106 从中序与后序遍历序列构造二叉树  https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0106.%E4%BB%8E%E4%B8%AD%E5%BA%8F%E4%B8%8E%E5%90%8E%E5%BA%8F%E9%81%8D%E5%8E%86%E5%BA%8F%E5%88%97%E6%9E%84%E9%80%A0%E4%BA%8C%E5%8F%89%E6%A0%91.md
+ * 根据一棵树的中序遍历与后序遍历构造二叉树。
+ * 例如，给出
+ *     中序遍历 inorder = [9,3,15,20,7]
+ *     后序遍历 postorder = [9,15,7,20,3]
+ *     就是以 后序数组的最后一个元素为切割点，先切中序数组，根据中序数组，反过来再切后序数组。一层一层切下去，每次后序数组最后一个元素就是节点元素。
+ *
  *
  */
 public class GenerateTree {
 
     Map<Integer,Integer> map;
+
     public TreeNode buildTree(int[] inorder,int[] postorder) {
         map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
@@ -30,6 +37,32 @@ public class GenerateTree {
         root.left = findNode(inorder,inBegin,rootIndex,postorder,postBegin,postBegin + lenOfLeft);
         root.right = findNode(inorder,rootIndex + 1,inEnd,postorder,postBegin + lenOfLeft,postEnd - 1);
         return root;
+    }
+
+    //从前序与中序遍历构造二叉树
+    Map<Integer,Integer> pre_in_map;
+    public TreeNode pre_in_buildTree(int[] preorder,int[] inorder) {
+        pre_in_map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i],i );
+        }
+
+        return pre_in_findNode(preorder,0,preorder.length,inorder,0,inorder.length);
+    }
+
+    private TreeNode pre_in_findNode(int[] preorder, int preBegin, int preEnd, int[] inorder, int inBegin, int inEnd) {
+        // 参数里的范围都是前闭后开
+        if (preBegin >= preEnd || inBegin >= inEnd) {
+            return null;
+        }
+
+        int rootIndex = pre_in_map.get(preorder[preBegin]);//// 找到前序遍历的第一个元素在中序遍历中的位置
+        TreeNode root = new TreeNode(inorder[rootIndex]);
+        int lenofLeft = rootIndex - inBegin; // 保存中序左子树个数，用来确定前序数列的个数
+        root.left = pre_in_findNode(preorder,preBegin + 1,preBegin + lenofLeft + 1,inorder,inBegin,rootIndex);
+        root.right = findNode(preorder,preBegin + lenofLeft + 1,preEnd,inorder,rootIndex + 1,inEnd);
+        return  root;
+
     }
 
 
---
+++ CodeCaprice/src/main/java/com/course/code/binaryTree/MergeTrees.java
@@ -0,0 +1,61 @@
+package com.course.code.binaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 617.合并二叉树  https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0617.%E5%90%88%E5%B9%B6%E4%BA%8C%E5%8F%89%E6%A0%91.md
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ *
+ */
+public class MergeTrees {
+
+    //递归
+    public TreeNode mergeTrees(TreeNode root1,TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return  root1;
+
+        root1.val = root1.val + root2.val;
+        root1.left = mergeTrees(root1.left,root2.left);
+        root1.right = mergeTrees(root1.right,root2.right);
+        return root1;
+    }
+
+    //队列迭代
+    public TreeNode mergeTreesByDeque(TreeNode root1,TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root1);
+        queue.offer(root2);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            // 此时两个节点一定不为空，val相加
+            node1.val = node1.val + node2.val;
+            // 如果两棵树左节点都不为空，加入队列
+            if (node1.left != null && node2.left != null) {
+                queue.offer(node1.left);
+                queue.offer(node2.left);
+            }
+
+            // 如果两棵树右节点都不为空，加入队列
+            if (node1.right != null && node2.right != null) {
+                queue.offer(node1.right);
+                queue.offer(node2.right);
+            }
+
+            //若node1的左节点为空，直接赋值
+            if (node1.left == null && node2.left != null) {
+                node1.left = node2.left;
+            }
+
+            //若node1的右节点为空，直接赋值
+            if (node1.right == null && node2.right != null) {
+                node1.right = node2.right;
+            }
+        }
+        return root1;
+    }
+}
