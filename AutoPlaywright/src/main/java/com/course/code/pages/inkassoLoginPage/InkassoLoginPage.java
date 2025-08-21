1f32de672716608ed977240addf384e004925e61
GuangGuangLi <15607521232@163.com>
2025-08-21 16:46:01 +0800

    数据驱动 支持json文件中配置多个测试参数

--- AutoPlaywright/src/main/java/com/course/code/pages/inkassoLoginPage/InkassoLoginPage.java
+++ AutoPlaywright/src/main/java/com/course/code/pages/inkassoLoginPage/InkassoLoginPage.java
@@ -16,6 +16,7 @@ public class InkassoLoginPage {
     private final Locator xiyi;
     private final Locator subButton;
     private final Locator userphone;
+    private final Locator failMsg;
 
 
     public InkassoLoginPage(Page page) {
@@ -26,6 +27,8 @@ public class InkassoLoginPage {
         this.xiyi = page.locator("form div").filter(new Locator.FilterOptions().setHasText("我已阅读并接受 《用户协议》 和 《隐私政策》")).locator("span").nth(1);
         this.subButton = page.getByRole(AriaRole.DIALOG).getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("登录/注册"));
         this.userphone = page.locator("xpath =//span[starts-with(@class,'el-dropdown-link')]");
+        this.failMsg = page.getByText("登录失败！请您检查验证码是否正确。");
+
 
     }
 
@@ -68,6 +71,11 @@ public class InkassoLoginPage {
     public String getUserphone() {
         logger.info("Getting user phone number");
         return userphone.innerText();
+    }
+
+    public boolean getFailMSg() {
+        logger.info("Getting fail message");
+        return failMsg.isVisible();
     }
 
     public void login(String number, String code) {
